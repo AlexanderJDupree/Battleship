0eec809252993e393db8b93786e62942103f264a
Alexander Dupree <alexanderjdupree@gmail.com>
2021-05-05 09:59:23 -0700

    Fix build error

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,7 +3,6 @@
  */
 export { default as Navigation } from './Navbar';
 export { default as Footer } from './Footer';
-export { default as Example } from './Example';
 export { default as ServerStats } from './ServerStats';
 export { default as Leaderboard } from './Leaderboard';
 export { default as SelectUsername } from './SelectUsername';
