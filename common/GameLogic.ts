68461ddf02712b567fcf39439afd78ae7a7a92ee
Joshua Carlson <jmcarlson0320@gmail.com>
2021-05-10 12:32:36 -0700

    outlined some basic functions and data structs

--- common/GameLogic.ts
+++ common/GameLogic.ts
@@ -1,5 +1,28 @@
+type Player = {
+  username: string;
+  board: number;
+  // other properties
+};
+
+type Hit = {
+  x: number;
+  y: number;
+};
+
 export class GameLogic {
+
+  players: Player[];
+
   constructor() {
-    console.log("Ive been constructed");
   }
+
+  addPlayer(player: Player) {
+  }
+
+  setBoard(board: number[][]) {
+  }
+
+  processHit(player: Player, hit: Hit) {
+  }
+
 }
