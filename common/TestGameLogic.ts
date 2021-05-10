0318fe84d0b3117e1cab3a0289a87274da653ac1
Joshua Carlson <jmcarlson0320@gmail.com>
2021-05-10 14:40:19 -0700

    constructor, some tests

--- common/TestGameLogic.ts
+++ common/TestGameLogic.ts
@@ -1,3 +1,12 @@
 import {GameLogic} from "./GameLogic";
 
+function printBoard(board: Number[][]) {
+  let j = board.length;
+  let i = board[0].length;
+  console.log(j + ', ' + i);
+}
+
 let GL = new GameLogic();
+console.log(GL);
+console.log(GL.player1);
+console.log(GL.player2);
