99d21ed2c77a0825eae9c27433c0d50d48836458
kkagawa88 <kkagawa@pdx.edu>
2021-05-25 18:40:25 -0700

    moving footer stuff in different branch

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,29 +7,5 @@ export default function Footer() {
   // TODO magic number, attempting to make the navbar non-fixed on smaller
   // screens here. This is what I came up with but it's mostly a hack.
   return (
-    <Navbar
-      expand='lg'
-      bg='theme-secondary-dark'
-      variant='dark'
-      as='footer'
-      className='mt-3'
-      // TODO get this footer to be fixed on short pages
-      //fixed={dimensions.height >= 955 ? 'bottom' : undefined}
-    >
-      <Container className='align-items-center justify-content-center'>
-        <p className='theme-text-secondary text-center'>
-          <div>
 
-          </div>
-          <div>
-            <h5>Contacts</h5>
-            <p>
-              Katherine Kagawa - github
-
-            </p>
-          </div>
-        </p>
-      </Container>
-    </Navbar>
-        );
       }
