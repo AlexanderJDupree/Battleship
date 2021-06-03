779bd1ff63e26a5826adc42c520c31f70946aa05
kkagawa88 <kkagawa@pdx.edu>
2021-06-02 18:30:57 -0700

    more compact version working

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,92 +7,87 @@ export default function Footer() {
   // TODO magic number, attempting to make the navbar non-fixed on smaller
   // screens here. This is what I came up with but it's mostly a hack.
   return (  
-    <div className="mt-5 footer">
-      <footer className="page-footer font-small bg-dark text-white">
-        <div className="container">
-
-          <div className="row text-center d-flex justify-content-center pt-5 mb-3">
-            <div className="col-md-2 mb-3">
-              <h6 className="text-uppercase font-weight-bold">
-                <a href="https://playbattleship.com/">
-                  Home
-                </a>
-              </h6>
-            </div>
-            <div className="col-md-2 mb-3">
-              <h6 className="text-uppercase font-weight-bold text-white text-underline">
-                <a href="https://playbattleship.com/about">
-                  About
-                </a>
-              </h6>
-            </div>
-          </div>
-
-          <hr className="hr_part"/>
-
-          <div className="row d-flex text-center justify-content-center mb-md-0 mb-4">
-            <div className="col-md-8 col-12 mt-5">
+    <div className="mt-2 footer">
+      <footer className="bg-dark text-white">
+        <div className="row text-center justify-content-center pt-2 mb-3">
+          <div className="col-2 mt-5">
               <h5 className="text-uppercase text font-weight-bold">
-                The Project
+                Links
               </h5>
-              <p className="foot_summary font-weight-light mt-3 col-lg-12 col-md-2">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio magnam sint, quae architecto rerum qui ea culpa, illum, nesciunt nihil minus maiores. Quos praesentium illo quae perferendis eveniet id rem.
-              </p>
+              <div className="mt-3 row-md-2 mb-3">
+                <h6 className="text-uppercase font-weight-bold">
+                  <a href="https://playbattleship.com/">
+                    Home
+                  </a>
+                </h6>
+              </div>
+              <div className="row-md-2 mb-3">
+                <h6 className="text-uppercase font-weight-bold text-white text-underline">
+                  <a href="https://playbattleship.com/about">
+                    About
+                  </a>
+                </h6>
+              </div>
             </div>
+
+          <div className="col-8 mt-5 text-left">
+            <h5 className="text-uppercase font-weight-bold">
+              The Project
+            </h5>
+            <p className="foot_summary  font-weight-light mt-3 ">
+              This website was a term project for CS465P Intro to Fullstack at Portland State University, 
+              Spring 2021. Playbattleship.com is a full-stack web application for playing the classic 
+              strategy game, Battleship, in the browser.
+            </p>
+          </div>
+        </div>
+
+        <hr className="hr_part pb-3"/>
+
+        <h6 className="text-uppercase text-center font-weight-bold mb-3">
+          Follow us on github!
+        </h6>
+        <section className="pb-3 row d-flex text-center justify-content-center mb-md-0">
+          
+          <div className="mt-2 col-lg-2 col-md-2 mb-4 mb-md-0">
+            <a className="btn btn-large btn-outline-light rounded-circle btn-floating m-1" 
+              href="https://github.com/kkagawa88" 
+              role="button"
+            >
+              <i className="fa fa-github"></i>
+            </a>
+            <span className="ml-2"> 
+              Alex
+            </span>
           </div>
 
-          <hr className="hr_part mt-5 mb-5"/>
+          <div className="mt-2 col-lg-2 col-md-2 mb-4 mb-md-0">
+            <a className="btn btn-large btn-outline-light rounded-circle btn-floating m-1" 
+              href="https://github.com/AlexanderJDupree" 
+              role="button"
+            >
+              <i className="fa fa-github"></i>
+            </a>
+            <span className="ml-2"> 
+              Josh
+            </span>
+          </div>
 
-          <h6 className="text-uppercase text-center font-weight-bold mb-3">
-            Follow us on github!
-          </h6>
+          <div className="mt-2 col-lg-2 col-md-2 mb-4 mb-md-0">
+            <a className="btn btn-large btn-outline-light rounded-circle btn-floating m-1" 
+              href="https://github.com/kkagawa88" 
+              role="button"
+            >
+              <i className="fa fa-github"></i>  
+            </a>
+            <span className="ml-2"> 
+              Katherine
+            </span>
+          </div>
+        </section>  
 
-          <section className="row d-flex text-center justify-content-center mb-md-0">
-            
-            <div className="mt-2 col-lg-2 col-md-2 mb-4 mb-md-0">
-              <a className="btn btn-large btn-outline-light rounded-circle btn-floating m-1" 
-                href="https://github.com/kkagawa88" 
-                role="button"
-              >
-                <i className="fa fa-github"></i>
-              </a>
-
-              <p>
-                Alex
-              </p>
-            </div>
-
-            <div className="mt-2 col-lg-2 col-md-2 mb-4 mb-md-0">
-              <a className="btn btn-large btn-outline-light rounded-circle btn-floating m-1" 
-                href="https://github.com/AlexanderJDupree" 
-                role="button"
-              >
-                <i className="fa fa-github"></i>
-              </a>
-
-              <p>
-                Josh
-              </p>
-            </div>
-
-            <div className="mt-2 col-lg-2 col-md-2 mb-4 mb-md-0">
-              <a className="btn btn-large btn-outline-light rounded-circle btn-floating m-1" 
-                href="https://github.com/kkagawa88" 
-                role="button"
-              >
-                <i className="fa fa-github"></i>
-              </a>
-
-              <p>
-                Katherine
-              </p>
-            </div>
-          </section>
-
-        </div>
       </footer>
-  
-  </div>
+    </div>
 
   );
 }
